48a628edadf710635d7b13791127394253f631b6
Saravanan <bprathap104@gmail.com>
2020-10-10 21:05:13 +0530

    notifier optimized

--- covid_notifier/covid_19_notifier.py
+++ covid_notifier/covid_19_notifier.py
@@ -2,6 +2,7 @@ import requests
 import notify2
 import time
 
+
 def update():
     covid_url='https://api.covid19india.org/data.json'
     r = requests.get(covid_url)
@@ -10,11 +11,12 @@ def update():
     notify2.init("Covid Notifier")
     n = notify2.Notification(None, icon='')
     n.set_urgency(notify2.URGENCY_NORMAL) 
-    n.set_timeout(10000) 
-
+    n.set_timeout(10000)
+    list_of_states=list(filter(lambda state:(state['statecode']=='TN' or state['statecode']=='WB'), statewise))
+    [ (n.update("Covid Notifier", f'{state["state"]} Status:\n {state["active"]}'), n.show(), time.sleep(5)) for state in list_of_states]
 
     # [ (n.update("Covid Notifier", f'{state["state"]} Status:\n {state["active"]}'), n.show(), time.sleep(5))  for state in statewise]
-    [ (n.update("Covid Notifier", f'{state["state"]} Status:\n {state["active"]}'), n.show(), time.sleep(5))  for state in statewise if state["statecode"] == 'WB' or state["statecode"] == 'TN']
+    # [ (n.update("Covid Notifier", f'{state["state"]} Status:\n {state["active"]}'), n.show(), time.sleep(5))  for state in statewise if state["statecode"] == 'WB' or state["statecode"] == 'TN']
     
     # for state in statewise:
     #     if state["statecode"] == 'WB' or state["statecode"] == 'TN':
@@ -23,3 +25,4 @@ def update():
     #         time.sleep(15)
 
 update()
+
